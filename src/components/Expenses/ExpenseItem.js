393939dd707e988a015b46d20f145dd7ad14f9ec
Chitranjan <chitranjan@Chitranjans-MacBook-Pro.local>
2021-11-28 20:39:44 +0530

    setting

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 import ExpenseDate from '../Expenses/ExpenseDate';
 import Card from '../UI/Card';
 import '../Expenses/ExpenseItem.css';
 
 
 const ExpenseItem = (props) => {
-  let title = props.title;
+  const [title,setTitle] = useState(props.title);
+
 
   const clickHandler = () => {
-    title = 'Updated !';
+    setTitle('Updated!');
   };
 
   return (
