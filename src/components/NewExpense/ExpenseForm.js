31f86463dd3dfe20a33c0a16e2e9802d2611f182
Chitranjan <chitranjan@Chitranjans-MacBook-Pro.local>
2021-12-01 09:08:58 +0530

    setting

---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ExpenseForm.css';
+
+const ExpenseForm = () =>{
+    return(
+        <div>
+            <form>
+                hello
+            </form>
+        </div>
+    );
+};
+
+
+export default ExpenseForm;
